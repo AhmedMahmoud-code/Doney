12a562d2e66dc3daeda23cfd0d38804914286f87
Ahmed Mahmoud <ahmedmahmoud.1@gmx.de>
2019-09-18 00:28:18 +0200

    Fix checkmarks and use Modell class

---
+++ doney/Modell/DateModell.swift
@@ -0,0 +1,15 @@
+//
+//  DateModell.swift
+//  doney
+//
+//  Created by Ahmed Mahmoud on 17.09.19.
+//  Copyright © 2019 Ahmed Mahmoud. All rights reserved.
+//
+
+import Foundation
+
+class Modell{
+    
+    var title : String = ""
+    var done : Bool = true
+}
